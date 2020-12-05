6171832e4d99843b2521af2239680fc4cc3ded43
derekjeter9 <64431821+derekjeter9@users.noreply.github.com>
2020-12-05 23:24:29 +0800

    Add files via upload

---
+++ 3 Nested Loop/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    double sum = 0.10;
+
+    for(i=2; i <= 15; i++)
+    {
+        sum = sum + sum;
+        printf("Total money for day %d is %.2f\n",i, sum);
+    }
+    return 0;
+}
